c60c06a4e49bb1b6a8329ea3d1233b9d6d831060
mazharul-islam321 <https://github.com/mazharul-islam321/School-Management-System.git>
2021-10-18 13:42:55 +0600

    update header,app and create some components like sign in, sign up

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Footer from "./components/Shared/Footer/Footer";
 import Header from "./components/Shared/Header/Header";
+import NotFound from "./components/NotFound/NotFound";
+import Signin from "./components/Signin/Signin";
+import Signup from "./components/Signup/Signup";
 
 function App() {
     return (
-        <div className="App">
-            <Header></Header>
-            <Home></Home>
-            <Footer></Footer>
-        </div>
+        <>
+            <Router>
+                <Header></Header>
+                <Switch>
+                    <Route exact path="/">
+                        <Home></Home>
+                    </Route>
+                    <Route path="/home">
+                        <Home></Home>
+                    </Route>
+                    <Route path="/signin">
+                        <Signin></Signin>
+                    </Route>
+                    <Route path="/signup">
+                        <Signup></Signup>
+                    </Route>
+                    <Route path="*">
+                        <NotFound></NotFound>
+                    </Route>
+                </Switch>
+                <Footer></Footer>
+            </Router>
+        </>
     );
 }
 
---
+++ src/components/Signin/Signin.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Signin = () => {
+    return (
+        <div>
+            <h2>this is sign in page</h2>
+        </div>
+    );
+};
+
+export default Signin;
