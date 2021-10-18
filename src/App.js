f49b6f48530c17b74f04dd7e5fa229b64956e7af
mazharul-islam321 <https://github.com/mazharul-islam321/School-Management-System.git>
2021-10-18 14:45:56 +0600

    update caruosel

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./components/Home/Home";
+import Home from "./components/Homepage/Home/Home";
 import Footer from "./components/Shared/Footer/Footer";
 import Header from "./components/Shared/Header/Header";
 import NotFound from "./components/NotFound/NotFound";
---
+++ src/components/Homepage/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Carousel from "../Carousels/Carousels";
+
+import "./Home.css";
+
+const Home = () => {
+    return (
+        <>
+            <Carousel></Carousel>
+        </>
+    );
+};
+
+export default Home;
